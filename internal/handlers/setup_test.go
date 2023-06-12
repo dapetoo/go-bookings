4d864221b24deee95da0360e7b3333cc219d7cf6
Peter DADA <dapetoo@gmail.com>
2023-06-12 23:46:20 +0100

    Setup Handler Test

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -1,1 +1,96 @@
 package handlers
+
+import (
+	"encoding/gob"
+	"github.com/alexedwards/scs/v2"
+	"github.com/dapetoo/go-bookings/internal/config"
+	"github.com/dapetoo/go-bookings/internal/models"
+	"github.com/dapetoo/go-bookings/internal/render"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/justinas/nosurf"
+	"log"
+	"net/http"
+	"time"
+)
+
+var session *scs.SessionManager
+var app config.AppConfig
+
+func getRoute() http.Handler {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	mux := chi.NewRouter()
+
+	// Middleware
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	//HTTP Server
+	mux.Get("/", Repo.Home)
+	mux.Get("/about", Repo.About)
+	mux.Get("/contact", Repo.Contact)
+	mux.Get("/service", Repo.Service)
+	mux.Get("/generals-quarters", Repo.Generals)
+	mux.Get("/majors-suite", Repo.Majors)
+
+	mux.Get("/search-availability", Repo.Availability)
+	mux.Post("/search-availability", Repo.PostAvailability)
+	mux.Post("/search-availability-json", Repo.AvailabilityJSON)
+
+	mux.Get("/make-reservation", Repo.Reservation)
+	mux.Post("/make-reservation", Repo.PostReservation)
+	mux.Get("/reservation-summary", Repo.ReservationSummary)
+
+	// File server
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
+
+// NoSurf is the csrf protection middleware
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   app.InProduction,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
+
+// SessionLoad loads and saves session data for current request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
