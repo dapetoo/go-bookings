c8d0b8c077059a78beb31304efb6b62c0888c48b
Peter DADA <dapetoo@gmail.com>
2023-06-13 00:38:33 +0100

    Setup Render test

--- internal/render/setup_test.go
+++ internal/render/setup_test.go
@@ -1,1 +1,32 @@
 package render
+
+import (
+	"github.com/alexedwards/scs/v2"
+	"github.com/dapetoo/go-bookings/internal/config"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
