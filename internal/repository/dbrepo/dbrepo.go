177186c0a207f7146d9220a7c56929aa319f04c7
Peter DADA <dapetoo@gmail.com>
2023-06-16 22:57:45 +0100

    Debugging

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -3,7 +3,6 @@ package dbrepo
 import (
 	"database/sql"
 	"github.com/dapetoo/go-bookings/internal/config"
-	"github.com/dapetoo/go-bookings/internal/repository"
 )
 
 type postgresDBRepo struct {
@@ -12,9 +11,9 @@ type postgresDBRepo struct {
 }
 
 // NewPostgresRepo creates a new repository
-func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) *postgresDBRepo {
 	return &postgresDBRepo{
-		App: a,
 		DB:  conn,
+		App: a,
 	}
 }
