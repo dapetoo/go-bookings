ab2d5aee3f64405dc8b3aee051ad3475a4497f00
Peter DADA <dapetoo@gmail.com>
2023-06-17 23:02:54 +0100

    Refactoring and clean up

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -18,186 +18,6 @@ import (
 	"time"
 )
 
-//// Repo the repository used by the handlers
-//var Repo *Repository
-//
-//// Repository is the repository type
-//type Repository struct {
-//	App *config.AppConfig
-//	DB  repository.DatabaseRepo
-//}
-//
-//// NewRepo creates a new repository
-//func NewRepo(a *config.AppConfig, db *driver.DB) *Repository {
-//	return &Repository{
-//		App: a,
-//		DB:  dbrepo.NewPostgresRepo(db.SQL, a),
-//	}
-//}
-//
-//// NewHandlers sets the repository for the handlers
-//func NewHandlers(r *Repository) {
-//	Repo = r
-//}
-//
-//// Home is the handler for the home page
-//func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-//	render.Template(w, r, "home.page.tmpl", &models.TemplateData{})
-//}
-//
-//func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-//	log.Println("Contact page")
-//	render.Template(w, r, "contact.page.tmpl", &models.TemplateData{})
-//}
-//
-//func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-//	log.Println("Contact page")
-//	render.Template(w, r, "about.page.tmpl", &models.TemplateData{})
-//}
-//
-
-//
-//// Reservation renders the make a reservation page and displays form
-//func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-//	var emptyReservation models.Reservation
-//	data := make(map[string]interface{})
-//	data["reservation"] = emptyReservation
-//
-//	render.Template(w, r, "make-reservation.page.tmpl", &models.TemplateData{
-//		Form: forms.New(nil),
-//		Data: data,
-//	})
-//}
-//
-//// PostReservation handles the posting of a reservation form
-//func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
-//	err := r.ParseForm()
-//	if err != nil {
-//		helpers.ServerError(w, err)
-//		return
-//	}
-//
-//	sd := r.Form.Get("start_date")
-//	ed := r.Form.Get("end_date")
-//
-//	// layout := "2006-01-02" // the date format must be in this format
-//	// 2020-01-01 01/02 03:04:05PM '06 -0700
-//
-//	layout := "2006-01-02"
-//
-//	startDate, err := time.Parse(layout, sd)
-//	if err != nil {
-//		helpers.ServerError(w, err)
-//		return
-//	}
-//
-//	endDate, err := time.Parse(layout, ed)
-//	if err != nil {
-//		helpers.ServerError(w, err)
-//		return
-//	}
-//
-//	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
-//	if err != nil {
-//		helpers.ServerError(w, err)
-//		return
-//	}
-//
-//	// grabbed data from whatever they entered in the field of the form
-//	reservation := models.Reservation{
-//		FirstName: r.Form.Get("first_name"),
-//		LastName:  r.Form.Get("last_name"),
-//		Email:     r.Form.Get("email"),
-//		Phone:     r.Form.Get("phone"),
-//		StartDate: startDate,
-//		EndDate:   endDate,
-//		RoomID:    roomID,
-//	}
-//
-//	// Validate the form
-//	form := forms.New(r.PostForm)
-//
-//	form.Required("first_name", "last_name", "email")
-//	form.MinLength("first_name", 3)
-//	form.IsEmail("email")
-//
-//	if !form.Valid() {
-//		data := make(map[string]interface{})
-//		data["reservation"] = reservation
-//		render.Template(w, r, "make-reservation.page.tmpl", &models.TemplateData{
-//			Form: form,
-//			Data: data,
-//		})
-//		return
-//	}
-//
-//	//Add reservation to the database
-//	newReservationID, err := m.DB.InsertReservation(reservation)
-//	if err != nil {
-//		helpers.ServerError(w, err)
-//		return
-//	}
-//
-//	restriction := models.RoomRestriction{
-//		StartDate:     startDate,
-//		EndDate:       endDate,
-//		RoomID:        roomID,
-//		ReservationID: newReservationID,
-//		RestrictionID: 1,
-//	}
-//
-//	err = m.DB.InsertRoomRestriction(restriction)
-//
-//	m.App.Session.Put(r.Context(), "reservation", reservation)
-//	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
-//}
-//
-//// Generals renders the room page
-//func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-//	render.Template(w, r, "generals.page.tmpl", &models.TemplateData{})
-//}
-//
-//// Majors renders the room page
-//func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-//	render.Template(w, r, "majors.page.tmpl", &models.TemplateData{})
-//}
-//
-//// Availability renders the search availability page
-//func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-//	render.Template(w, r, "search-availability.page.tmpl", &models.TemplateData{})
-//}
-//
-//// PostAvailability handles post
-//func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
-//	start := r.Form.Get("start")
-//	end := r.Form.Get("end")
-//
-//	w.Write([]byte(fmt.Sprintf("start date is %s and end is %s", start, end)))
-//}
-//
-//type jsonResponse struct {
-//	OK      bool   `json:"ok"`
-//	Message string `json:"message"`
-//}
-//
-//// AvailabilityJSON handles request for availability and sends JSON response
-//func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
-//	resp := jsonResponse{
-//		OK:      true,
-//		Message: "Available!",
-//	}
-//
-//	out, err := json.MarshalIndent(resp, "", "     ")
-//	if err != nil {
-//		helpers.ServerError(w, err)
-//		return
-//	}
-//
-//	w.Header().Set("Content-Type", "application/json")
-//	w.Write(out)
-//}
-//
-
 // Repo the repository used by the handlers
 var Repo *Repository
 
@@ -215,6 +35,14 @@ func NewRepo(a *config.AppConfig, db *driver.DB) *Repository {
 	}
 }
 
+// NewTestRepo creates a new repository
+func NewTestRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+		DB:  dbrepo.NewTestingsRepo(a),
+	}
+}
+
 // NewHandlers sets the repository for the handlers
 func NewHandlers(r *Repository) {
 	Repo = r
@@ -222,10 +50,7 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-
-	// call renderTemplate
 	render.Template(w, r, "home.page.tmpl", &models.TemplateData{})
-
 }
 
 // About is the handler for the about page
--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -18,3 +18,11 @@ func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo 
 		DB:  conn,
 	}
 }
+
+// NewPostgresRepo creates a new repository
+func NewTestingsRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
