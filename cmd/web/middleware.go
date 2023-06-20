5f662775dceab828fa5741ca98df8d0a47f7879e
Peter DADA <dapetoo@gmail.com>
2023-06-20 08:47:24 +0100

    Auth middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/dapetoo/go-bookings/internal/helpers"
 	"github.com/justinas/nosurf"
 	"net/http"
 )
@@ -30,4 +31,16 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves session data for current request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+// Auth checks if user is authenticated
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -473,8 +473,13 @@ func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
 	email := r.Form.Get("email")
 	password := r.Form.Get("password")
 	form.Required("email", "password")
+	form.IsEmail("email")
 	if !form.Valid() {
 		// TODO: Take user back to login page and show them the errors
+		render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+			Form: form,
+		})
+		return
 	}
 
 	id, _, err := m.DB.Authenticate(email, password)
